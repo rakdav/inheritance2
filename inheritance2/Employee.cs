786693ba5cb30866f13f546a1e6e6ccb4de4edac
John Doe <gogs@fake.local>
2022-02-16 15:16:24 +0200

    Добавьте файлы проекта.

---
+++ inheritance2/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance2
+{
+    internal class Employee:Human
+    {
+        private double salary;
+
+        public Employee(string fName,string lName,double salary):base(fName,lName)
+        {
+            this.salary = salary;
+        }
+        public Employee(string fName, string lName,DateTime birth, double salary) : base(fName,lName,birth)
+        {
+            this.salary = salary;
+        }
+    }
+}
---
+++ inheritance2/Human.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance2
+{
+    internal class Human
+    {
+        private string _firstName;
+        private string _lastName;
+        private DateTime _birthDay;
+
+        public Human(string firstName, string lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+
+        public Human(string firstName, string lastName, DateTime birthDay) : this(firstName, lastName)
+        {
+            _birthDay = birthDay;
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Фамилия:{_firstName}\n" +
+                $"Имя:{_lastName}\n" +
+                $"Дата рождения:{_birthDay}");
+        }
+    }
+}
